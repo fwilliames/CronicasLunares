a87c47e1b34fd2d81d679896d2f75f495528cafd
Frank Williames <dev.frankw@gmail.com>
2023-08-24 19:27:00 -0400

    documentando o modulo nodeLoader

--- modules/nodeLoader.lua
+++ modules/nodeLoader.lua
@@ -1,25 +1,52 @@
 local nodeLoader = {}
-local initialNode = nil ---@type Node
 
-local nodeDictionay = {} ---@type table<string,Node>
+    --[[
+        Variable that represents the initial node for navigation in the game.
 
-local function loadNode(path)
-    local node = require(path)
+        Type:
+        - initialNode: An object of type Node.
+    ]]
+    local initialNode = nil ---@type Node
 
-    if node == nil then
-        warn("Falha ao carregar, o node nao existe")
-        return
+    --[[
+        Variable that represents a dictionary of nodes with string keys.
+
+        Type:
+        - nodeDictionary: A table that associates strings with objects of type Node.
+    ]]
+    local nodeDictionay = {} ---@type table<string,Node>
+
+    --[[
+        Loads a node from a path and adds it to the node dictionary.
+
+        Parameters:
+        - path: The path to the node file.
+
+        Usage:
+        loadNode(path)
+    ]]
+    local function loadNode(path)
+        local node = require(path)
+
+        if node == nil then
+            warn("Falha ao carregar, o node nao existe")
+            return
+        end
+
+        if nodeDictionay[node.id] ~= nil then
+            warn("Falha ao carregar, o node ja existe")
+            return
+        end
+
+        nodeDictionay[node.id] = node
+        
     end
+    --[[
+        Loads the game nodes and adds them to the node dictionary.
 
-    if nodeDictionay[node.id] ~= nil then
-        warn("Falha ao carregar, o node ja existe")
-        return
-    end
-
-    nodeDictionay[node.id] = node
-    
-end
-    ---Load all the nodes internally
+        Usage:
+        nodeLoader.loadNodes()
+    ]]
     function nodeLoader.loadNodes()
         nodeDictionay = {}
 
@@ -34,21 +61,48 @@ end
         
     end
 
-    ---Return all nodes created by this script
+    --[[
+        Gets the dictionary of loaded nodes.
+
+        Returns:
+        - A dictionary containing the loaded nodes.
+
+        Usage:
+        local nodes = nodeLoader.getNodes()
+    ]]
     function nodeLoader.getNodes()
         return nodeDictionay 
         
     end
 
-    ---Return the nodeID's associated node
+    --[[
+        Gets a specific node based on its ID.
+
+        Parameters:
+        - nodeID: The ID of the node to retrieve.
+
+        Returns:
+        - The node corresponding to the ID.
+
+        Usage:
+        local node = nodeLoader.getNode(nodeID)
+    ]]
     function nodeLoader.getNode(nodeID)
         return nodeDictionay[nodeID]
         
     end
 
+    --[[
+        Gets the loaded initial node.
+
+        Returns:
+        - The initial node.
+
+        Usage:
+        local initialNode = nodeLoader.getInitialNode()
+    ]]
     function nodeLoader.getInitialNoder()
         return initialNode
     end
-
 
 return nodeLoader    
