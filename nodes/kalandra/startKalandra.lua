208381b72b5e6ec6dd959fe45795b0066f9fca28
Frank Williames <dev.frankw@gmail.com>
2023-08-24 17:44:35 -0400

    criando node NYFF e Kalandra

---
+++ nodes/kalandra/startKalandra.lua
@@ -0,0 +1,44 @@
+local Node = require("class.Node")
+local Choice = require("class.choice")
+
+local START_ID <const> = "startKalandra"
+
+--Create the Start Node
+local startKalandra = Node:new(startID) ---@type Node
+    startKalandra.title = "Praias de Kalandra"   
+    startKalandra.description = "As ondas dancam e as brisas voam enquanto o sol te acolhe com um calor agradavel. Eh um lugar belo como dizem as lendas. Porem algo esta errado... nao tem ninguem aqui"
+    startKalandra.header =[[
+██████╗ ██████╗  █████╗ ██╗ █████╗ ███████╗    ██████╗ ███████╗    ██╗  ██╗ █████╗ ██╗      █████╗ ███╗   ██╗██████╗ ██████╗  █████╗ 
+██╔══██╗██╔══██╗██╔══██╗██║██╔══██╗██╔════╝    ██╔══██╗██╔════╝    ██║ ██╔╝██╔══██╗██║     ██╔══██╗████╗  ██║██╔══██╗██╔══██╗██╔══██╗
+██████╔╝██████╔╝███████║██║███████║███████╗    ██║  ██║█████╗      █████╔╝ ███████║██║     ███████║██╔██╗ ██║██║  ██║██████╔╝███████║
+██╔═══╝ ██╔══██╗██╔══██║██║██╔══██║╚════██║    ██║  ██║██╔══╝      ██╔═██╗ ██╔══██║██║     ██╔══██║██║╚██╗██║██║  ██║██╔══██╗██╔══██║
+██║     ██║  ██║██║  ██║██║██║  ██║███████║    ██████╔╝███████╗    ██║  ██╗██║  ██║███████╗██║  ██║██║ ╚████║██████╔╝██║  ██║██║  ██║
+╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚══════╝    ╚═════╝ ╚══════╝    ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝
+
+                                            _\/_                 |                _\/_
+                                            /o\\             \       /            //o\
+                                             |                 .---.                |
+                                            _|_______     --  /     \  --     ______|__
+                                                    `~^~^~^~^~^~^~^~^~^~^~^~`
+                                                        jgs
+    ]]
+    startKalandra.choices = {}
+    --[[
+    --Create the Choices
+    table.insert(
+        startKalandra.choices,
+        Choice:new(
+        "startKalandra",
+        "Para Praia Ensolarada"
+        )
+    )
+
+    table.insert(
+        startKalandra.choices,
+        Choice:new(
+        "startKalandra",
+        "Para a Montanha Gelada"
+        )
+    )]]
+
+return startKalandra
---
+++ nodes/nyff/starNyff.lua
@@ -0,0 +1,45 @@
+local Node = require("class.Node")
+local Choice = require("class.choice")
+
+local START_ID <const> = "startNyff"
+
+--Create the Start Node
+local startNyff = Node:new(startID) ---@type Node
+    startNyff.title = "Montanha de Nyff"   
+    startNyff.description = "Esta frio a neve esta por todo lugar e o silencio esta ensurdecedor, vc percebe que esqueceu sua mochila em casa, sua unica comida eh uma batata..."
+    startNyff.header =[[
+    ███╗   ███╗ ██████╗ ███╗   ██╗████████╗ █████╗ ███╗   ██╗██╗  ██╗ █████╗ ███████╗    ██████╗ ███████╗    ███╗   ██╗██╗   ██╗███████╗███████╗
+    ████╗ ████║██╔═══██╗████╗  ██║╚══██╔══╝██╔══██╗████╗  ██║██║  ██║██╔══██╗██╔════╝    ██╔══██╗██╔════╝    ████╗  ██║╚██╗ ██╔╝██╔════╝██╔════╝
+    ██╔████╔██║██║   ██║██╔██╗ ██║   ██║   ███████║██╔██╗ ██║███████║███████║███████╗    ██║  ██║█████╗      ██╔██╗ ██║ ╚████╔╝ █████╗  █████╗  
+    ██║╚██╔╝██║██║   ██║██║╚██╗██║   ██║   ██╔══██║██║╚██╗██║██╔══██║██╔══██║╚════██║    ██║  ██║██╔══╝      ██║╚██╗██║  ╚██╔╝  ██╔══╝  ██╔══╝  
+    ██║ ╚═╝ ██║╚██████╔╝██║ ╚████║   ██║   ██║  ██║██║ ╚████║██║  ██║██║  ██║███████║    ██████╔╝███████╗    ██║ ╚████║   ██║   ██║     ██║     
+    ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝    ╚═════╝ ╚══════╝    ╚═╝  ╚═══╝   ╚═╝   ╚═╝     ╚═╝   
+                                                        _    .  ,   .           .
+                                                    *  / \_ *  / \_      _  *        *   /\'__        *
+                                                      /    \  /    \,   ((        .    _/  /  \  *'.
+                                                 .   /\/\  /\/ :' __ \_  `          _^/  ^/    `--.
+                                                    /    \/  \  _/  \-'\      *    /.' ^_   \_   .'\  *
+                                                   /\  .-   `. \/     \ /==~=-=~=-=-;.  _/ \ -. `_/   \
+                                                  /  `-.__ ^   / .-'.--\ =-=~_=-=~=^/  _ `--./ .-'  `-
+                                                /jgs     `.  / /       `.~-^=-=~=^=.-'      '-._ `._
+    ]]
+    startNyff.choices = {}
+    --[[
+    --Create the Choices
+    table.insert(
+        startNyff.choices,
+        Choice:new(
+        "startKalandra",
+        "Para Praia Ensolarada"
+        )
+    )
+
+    table.insert(
+        startNyff.choices,
+        Choice:new(
+        "startNyff",
+        "Para a Montanha Gelada"
+        )
+    )]]
+
+return startNyff
