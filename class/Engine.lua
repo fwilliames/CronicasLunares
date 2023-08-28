5fd8c07b372a8f0ced62097f1402dbf6bf822226
Frank Williames <dev.frankw@gmail.com>
2023-08-28 02:22:27 -0400

    documentação das funções da classe engine

--- class/Engine.lua
+++ class/Engine.lua
@@ -9,9 +9,22 @@ local Engine = class("Engine")
     function Engine:initialize()
     end
 
+    --[[ 
+        Runs the main game loop that handles player interaction and navigation between nodes.
+        
+        This function continuously displays the content of the current node, shows the available choices
+        to the player, and handles their input to navigate to the next node.
+
+        The loop continues until the game is over.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+    ]]
     function Engine:runMainLoop()
         
-
         while not game.isOver do
             os.execute("cls")
             local node = game.activeNode
@@ -34,22 +47,43 @@ local Engine = class("Engine")
             --game.isOver = true -- para encerrar o game 
         end
 
-       
     end
 
+    --[[ 
+        Displays the content of a given node, including its header if available.
+        
+        This function prints the header (if provided), title, description, and card limit around the content.
+
+        Parameters:
+        - node: The node to display.
+
+        Returns:
+        None
+    ]]
     function Engine:printNode(node)
         if node.header then
             utils.cardLimite()
             print()
             print(node.header)
         end
-        
         print(node.title)
         print(node.description)
         utils.cardLimite()
-        
     end
     
+    --[[ 
+        Retrieves the valid choices available for a given node.
+        
+        This function iterates through the choices of the provided node, checks if each choice has a condition,
+        and if it does, evaluates the condition. If a choice has no condition or its condition evaluates to true,
+        the choice is considered valid and added to the result.
+
+        Parameters:
+        - node: The node for which to retrieve valid choices.
+
+        Returns:
+        - result: A table containing the valid choices.
+    ]]
     function Engine:getValidChoices(node)
         local result = {}
         for _, choice in pairs(node.choices) do
@@ -60,6 +94,18 @@ local Engine = class("Engine")
         return result     
     end
 
+    --[[ 
+        Displays the list of available choices to the player.
+        
+        This function iterates through the provided list of choices and prints each choice's description along
+        with its corresponding index.
+
+        Parameters:
+        - listChoices: The list of choices to display.
+
+        Returns:
+        None
+    ]]
     function Engine:showChoices(listChoices)
         for i, choice in pairs(listChoices) do
             print(
@@ -69,6 +115,18 @@ local Engine = class("Engine")
 
     end
 
+    --[[ 
+        Asks the player for input and validates the response.
+        
+        This function prompts the player for input and validates whether the entered value is a valid number within
+        the specified range. The function continues to prompt the player until a valid input is received.
+
+        Parameters:
+        - amount: The upper limit of the valid input range.
+
+        Returns:
+        - answer: The valid input received from the player.
+    ]]
     function Engine:askForInput(amount)
         while true do
             io.write("> ")
@@ -80,8 +138,7 @@ local Engine = class("Engine")
                 return answer
             end
             io.write("Resposta invalida, tente novamente \n")
-        end
-       
+        end    
     end
 
 return Engine
